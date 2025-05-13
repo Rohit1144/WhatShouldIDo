494c58a1234d18b4b6b989fe3cd081d272cdd7ae
Yiheng.Cai <ycai0085@student.monash.edu>
2025-05-13 22:16:01 +1000

    feat: implement AppDatabase for Room database

---
+++ app/src/main/java/com/example/fit5046_g4_whatshouldido/data/local/db/AppDatabase.kt
@@ -0,0 +1,42 @@
+package com.example.fit5046_g4_whatshouldido.data.local.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.fit5046_g4_whatshouldido.data.local.dao.TaskDAO
+import com.example.fit5046_g4_whatshouldido.data.local.entity.*
+
+@Database(
+    entities = [
+        Task::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDAO
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        private const val DB_NAME = "app_db"
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DB_NAME
+                )
+                    .fallbackToDestructiveMigration(true)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+}
