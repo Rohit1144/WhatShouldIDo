948b2a2c509dbe2a1fbfe1aa74f87f3e5cee772d
Yiheng.Cai <ycai0085@student.monash.edu>
2025-05-16 14:55:25 +1000

    feat: Implement quote saving and deletion in QuotesRepository

--- app/src/main/java/com/example/fit5046_g4_whatshouldido/data/repository/QuotesRepository.kt
+++ app/src/main/java/com/example/fit5046_g4_whatshouldido/data/repository/QuotesRepository.kt
@@ -1,9 +1,12 @@
 package com.example.fit5046_g4_whatshouldido.data.repository
 
+import com.example.fit5046_g4_whatshouldido.data.local.dao.QuoteDAO
+import com.example.fit5046_g4_whatshouldido.data.local.entity.Quote
 import com.example.fit5046_g4_whatshouldido.data.network.RetrofitObject
 import com.example.fit5046_g4_whatshouldido.models.QuoteModel
 
-class QuotesRepository {
+class QuotesRepository  (private val dao: QuoteDAO)
+{
     suspend fun getNewQuote(): QuoteModel? {
         return try {
             val result = RetrofitObject.api.getRandomQuote()
@@ -11,5 +14,13 @@ class QuotesRepository {
         } catch (e: Exception) {
             null
         }
+    }
+
+    suspend fun saveQuote(quote: QuoteModel) {
+        dao.insert(Quote(text = quote.q, author = quote.a))
+    }
+
+    suspend fun deleteQuote(quote: Quote) {
+        dao.delete(quote)
     }
 }
