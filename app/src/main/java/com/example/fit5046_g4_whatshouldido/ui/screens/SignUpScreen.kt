0f47ce689f73851f3dcfe7be2e0672ec58f4807d
Yiheng.Cai <ycai0085@student.monash.edu>
2025-05-22 00:29:29 +1000

    Fix: Minor code cleanup in SignUpScreen

--- app/src/main/java/com/example/fit5046_g4_whatshouldido/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/fit5046_g4_whatshouldido/ui/screens/SignUpScreen.kt
@@ -1,11 +1,6 @@
 package com.example.fit5046_g4_whatshouldido.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.rememberDatePickerState
-import java.time.Instant
-import java.time.ZoneId
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -21,6 +16,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,16 +26,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -50,17 +46,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.fit5046_g4_whatshouldido.Managers.AuthResponse
 import com.example.fit5046_g4_whatshouldido.Managers.AuthenticationManager
 import com.example.fit5046_g4_whatshouldido.R
 import com.example.fit5046_g4_whatshouldido.validations.FormValidation
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.Calendar
+import java.time.Instant
+import java.time.ZoneId
 
 @SuppressLint("DefaultLocale")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -308,7 +306,7 @@ fun SignUp(navController: NavController) {
             Text(text = "Already have an account? Sign In",
                 style = MaterialTheme.typography.bodySmall,
                 color = colorResource(R.color.light_red),
-                modifier = Modifier.drawBehind() {
+                modifier = Modifier.drawBehind {
                     val strokeWidthPx = 1.dp.toPx()
                     val verticalOffset = size.height - 2.sp.toPx()
                     drawLine(
