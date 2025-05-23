8ed7c38a3722f26dd13c9a8b16ce7665560529ed
Yiheng.Cai <ycai0085@student.monash.edu>
2025-05-23 18:22:53 +1000

    Refactor: Remove AI response generation from ViewModel

--- app/src/main/java/com/example/fit5046_g4_whatshouldido/viewmodel/AiViewModel.kt
+++ app/src/main/java/com/example/fit5046_g4_whatshouldido/viewmodel/AiViewModel.kt
@@ -1,24 +1,11 @@
 package com.example.fit5046_g4_whatshouldido.viewmodel
 
 import android.app.Application
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.fit5046_g4_whatshouldido.LocalLLMModel.GemmaLocalInference
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class AiViewModel(application: Application) : AndroidViewModel(application){
     val isInitialized = mutableStateOf(GemmaLocalInference.isReady())
 
-    private val _response = mutableStateOf("No input yet.")
-    val response: State<String> = _response
-
-    fun generate(prompt: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val output = GemmaLocalInference.generate(prompt)
-            _response.value = output
-        }
-    }
 }
